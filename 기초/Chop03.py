c78d55f9dbc1d3c4b10ad9be07b2901fe98576cc
sumin2123 <mini247269@naver.com>
2021-04-12 12:05:43 +0900

    커밋

---
+++ 기초/Chop03.py
@@ -0,0 +1,27 @@
+# 변수
+rainbow = '빨주노초파남보'
+print(rainbow)
+
+#변수에 저장한 겂을 변경하는 코드
+count = 0
+print(count)
+
+count = 1
+print(count)
+
+count = count + 1
+print(count)
+
+# Q1.  커피, 주스, 홍차 가격을 각각 저장하고, 변수를 사용해 주문에 맞게 계산하고 금액을 출력하기
+# 주문서 1 - 커피3잔 , 주스 2잔, 홍차 1잔
+# 주문서 2 - 커피4잔 , 주스 3잔, 홍차 3잔
+# 주문서 3 - 커피1잔 , 주스 1잔, 홍차 2잔
+
+coffee = 4100 #커피의 가격
+juice = 4600 #주스의 가격
+tea = 3900 #홍차의 가격
+
+print(coffee * 3 + juice * 2 + tea * 1)
+print(coffee * 4 + juice * 3 + tea * 3)
+print(coffee * 1 + juice * 1 + tea * 2)
+
---
+++ 기초/상자에 사탕담기.py
@@ -0,0 +1,15 @@
+candy0 = '딸기맛'
+candy1 = '레몬맛'
+candy2 = '수박맛'
+candy3 = '박하맛'
+candy4 = '우유맛'
+candy5 = '땅콩맛'
+
+print(candy0) #딸기맛
+print(candy1) #레몬맛
+print(candy2) #수박맛
+print(candy3) #박하맛
+print(candy4) #우유맛
+print(candy5) #땅콩맛
+
+
